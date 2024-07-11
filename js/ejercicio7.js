720c22a9ae4d11452ee7ea26635918409d802fb4
Eugenia Sansierra <eugesans@gmail.com>
2024-07-11 00:14:09 -0300

    se quedo en ejercicio7 a medias

---
+++ js/ejercicio7.js
@@ -0,0 +1,4 @@
+let num1 = parseInt(prompt("Ingrese primer número:"));
+let num2 = parseInt(prompt("Ingrese segundo número:"));
+let num 3 = parseInt(prompt("Ingrese tercer número"));
+if (num1>num2 && ) 
